fb6d4253cce11845a2a2a128ba0143168f2931b9
Guilherme Faria <guilherme.ap404@gmail.com>
2024-11-08 22:05:47 -0300

    Att Repository

--- management/src/main/java/com/erp/management/domain/repository/SupplierOrderRepository.java
+++ management/src/main/java/com/erp/management/domain/repository/SupplierOrderRepository.java
@@ -1,9 +1,8 @@
 package com.erp.management.domain.repository;
 
 import com.erp.management.domain.model.SupplierOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SupplierOrderRepository extends JpaRepository<SupplierOrder, Long> {
+public interface SupplierOrderRepository extends BaseRespository<SupplierOrder, Long> {
 }
